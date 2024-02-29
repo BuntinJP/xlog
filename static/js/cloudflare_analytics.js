a97bf7979fd79ab786427faf996be423ba4c904d
Buntin-Synthia <mail@buntin.xyz>
2024-02-29 19:29:10 +0900

    Zaraz,Cloudflare Web Analytics追加

---
+++ static/js/cloudflare_analytics.js
@@ -0,0 +1,14 @@
+/*
+  xlog :: static/js/cloudflare_analytics.js
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+  var script = document.createElement('script');
+  script.defer = true;
+  script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+  script.setAttribute(
+    'data-cf-beacon',
+    '{"token": "7d41e2bf4b29401d9c46d2b5ad807a2b"}'
+  );
+  document.body.appendChild(script);
+});
