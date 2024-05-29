4831af95b86456c8cf7480b2d17ac927672524c3
Buntin-LArchel <mail@buntinjp.net>
2024-05-29 21:22:46 +0900

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className='mt-auto py-10 text-center text-medium'>
       <p className='mb-2 text-[#a89984]'>
-        Powered by Next.js<span className='mx-3 text-[#a89984]'>|</span>Hosted
+        Powered by Next.js & fumadocs<span className='mx-3 text-[#a89984]'>|</span>Hosted
         by zeabur
       </p>
       <p className='text-[#a89984]'>&copy; 2024 Buntin Synthia</p>
