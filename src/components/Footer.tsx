dc72690d17a907f7ef77ea4a3a8a2280d7cca3a2
Buntin-Synthia <mail@buntin.xyz>
2026-01-26 23:55:14 +0900

    fumadocs => Fumadocs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className='mt-auto py-10 text-center text-medium'>
       <p className='mb-2 text-[#a89984]'>
-        Powered by Next.js & fumadocs
+        Powered by Next.js & Fumadocs
         <span className='mx-3 text-[#a89984]'>|</span>Hosted by Vercel
       </p>
       <p className='text-[#a89984]'>&copy; 2026 Buntin Synthia</p>
