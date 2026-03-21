540eb8605ed6268bff076a26f2736833bab137f8
Buntin-Synthia <mail@buntin.xyz>
2026-03-22 04:12:11 +0900

    sitemap.xml fix

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,6 +1,9 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
+import type { IConfig } from 'next-sitemap';
+
+const config: IConfig = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL ?? 'https://www.xlog.systems',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
+
+export default config;
