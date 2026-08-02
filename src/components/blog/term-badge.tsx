709ef177616eba05236db8b081b387ddb0c98fca
Buntin-Synthia <mail@buntin.xyz>
2026-08-03 01:40:06 +0900

    fix: preserve taxonomy badge icon color

--- src/components/blog/term-badge.tsx
+++ src/components/blog/term-badge.tsx
@@ -16,7 +16,7 @@ export function TermBadge({
       href={termPath(basePath, name)}
       className="flex rounded-lg bg-[#32302f] px-2 text-base no-underline hover:underline"
     >
-      <Icon size={18} className="my-auto mr-1" />
+      <Icon size={18} className="my-auto mr-1 text-[#fbf1c7]" />
       <span className="text-blue-400 hover:text-blue-300">{name}</span>
     </Link>
   );
